a6f5f3c1b71b72c2df24ab3b77776bc75a6c1915
Muhammad Awais <second2kamali@gmail.com>
2025-10-18 21:01:49 -0700

    Update student.js

--- src/config/student.js
+++ src/config/student.js
@@ -1,4 +1,4 @@
 // Edit these two fields with your details before you start.
 // They will be shown at the very top of the screen.
 export const STUDENT_NAME = "Muhammad Awais";
-export const REG_NO = "SP23-BSE-020";
+export const REG_NO = "SP23-BSE-02";
