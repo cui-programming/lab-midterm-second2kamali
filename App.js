52ba0c11ad5114f26796784394b0c777a0d9de1e
Muhammad Awais <second2kamali@gmail.com>
2025-10-18 21:16:00 -0700

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { View, ScrollView } from 'react-native';
 import { styles } from './src/styles/styles';
 import AboutMe from './src/components/custom/AboutMe';
 import TasbihList from './src/components/custom/TasbihList';
-// import TeacherMessage from './src/components/custom/TeacherMessage'; // Students create this
+import TeacherMessage from './src/components/custom/TeacherMessage'; // Students create this
 import SearchAndAdd from './src/components/custom/SearchAndAdd';
 import { STUDENT_NAME, REG_NO } from './src/config/student';
 
@@ -12,6 +12,7 @@ export default function App() {
     <ScrollView contentContainerStyle={styles.container}>
       <AboutMe name={STUDENT_NAME} regNo={REG_NO} />
       {/* TODO: after creating the custom/TeacherMessage component, render it here */}
+      <TeacherMessage />
       <TasbihList />
       <SearchAndAdd />
     </ScrollView>
